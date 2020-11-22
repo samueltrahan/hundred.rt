070662be78c7153c1bd870702eb1c950bb88ffc1
Samuel Trahan <samtrahan91@gmail.com>
2020-11-21 20:11:29 -0600

    banner component made

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import SideBar from './components/SideBar/SideBar';
+import Banner from './components/Banner/Banner';
+import Footer from './components/Footer/Footer';
 
 export default function App() {
   return (
     <div>
       <NavBar />
       <SideBar />
+      <Banner />
+      <Footer />
     </div>
   )
 }
